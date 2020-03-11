5b10b017e54091599d767227534bc868a02a567c
maxharder <maxharder@hotmail.de>
2020-03-11 20:42:31 +0100

    added Background

---
+++ Prototyp Game/Assets/Scripts/ScrollBackgroundScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollBackgroundScript : MonoBehaviour
+{
+    float curx;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        curx = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= -1f)
+        {
+            transform.position = new Vector3(transform.position.x, 19, transform.position.z);
+        }
+        else
+        {
+            curx = transform.position.y - 0.05f;
+            transform.position = new Vector3(transform.position.x, curx, transform.position.z);
+        }
+    }
+}
